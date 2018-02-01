aa0515ccb6026ee6998377a55516e63b36ba9f73
Joe Thomas <joe@Joes-MBP.local>
2018-02-01 10:27:29 -0600

    v1.1.9 - Various changes
    - Add to list of WP default post types.
    - Fixed a CSS style.

--- includes/wpbakery-mods.php
+++ includes/wpbakery-mods.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * Enable WPBakery Page Builder for Uncode Content Blocks
+ * Enable WPBakery Page Builder for custom post types, since the WPBakery Role Editor settings are not sticking.
  *
- * @since 1.1.7
+ * @since 1.1.9
  */
 function joe_ucm_enable_wpbpb_on_uncode_content_block() {
 
 	if( function_exists( 'vc_set_default_editor_post_types' ) ) {
 
 		$list = array(
+			'page',
+			'post',
 			'uncodeblock',
+			'shopify_product',
+			'influencer',
+			'deal',
 		);
 
 		vc_set_default_editor_post_types( $list );
